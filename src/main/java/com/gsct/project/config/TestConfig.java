e2d0d885aaf23119fcb935364387fc7ad609f8d0
JVitorAS <jo0819637@outlook.com>
2023-08-15 10:22:32 -0300

    upload order/user

--- src/main/java/com/gsct/project/config/TestConfig.java
+++ src/main/java/com/gsct/project/config/TestConfig.java
@@ -1,5 +1,6 @@
 package com.gsct.project.config;
 
+import java.time.Instant;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,7 +8,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.gsct.project.entities.Order;
 import com.gsct.project.entities.User;
+import com.gsct.project.repositories.OrderRepository;
 import com.gsct.project.repositories.UserRepository;
 
 @Configuration
@@ -16,6 +19,11 @@ public class TestConfig implements CommandLineRunner{
 
 	@Autowired
 	private UserRepository userRepository;
+	
+	@Autowired
+	private OrderRepository orderRepository;
+	
+	
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -23,8 +31,19 @@ public class TestConfig implements CommandLineRunner{
 		User u1 = new User(null, "Maria Bener", "Maria@email.com", "981701212", "123456");
 		User u2 = new User(null, "Alex Green", "Alex@email.com", "977772898", "654321");
 		
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07z"), u1);
+		Order o2 = new Order(null, Instant.parse("2019-06-20T19:53:07z"), u2);
+		Order o3 = new Order(null, Instant.parse("2019-06-20T19:21:07Z"), u2);
+		
 		userRepository.saveAll(Arrays.asList(u1, u2));
+		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+		
 	}
+	
+	
+
+	
+	
 	
 	
 }
